ad30a83dc04e8c55e2ead3c3fa25ae7fc49af369
Tirath Singh <stirath462002@gmail.com>
2025-08-24 13:59:57 +0530

    eighth commit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 // app config
 const app = express();
-const port = process.env.PORT||4000;
+const PORT = process.env.PORT||4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // middleware
